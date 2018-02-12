021cacca21d32e485352bf07c6e0ab84472da6cb
Masai Young <masai.young@gmail.com>
2018-02-12 15:17:58 -0500

    Setup project structure

---
+++ FlashcardHW/Model/Flashcard.swift
@@ -0,0 +1,11 @@
+//
+//  Flashcard.swift
+//  FlashcardHW
+//
+//  Created by Masai Young on 2/12/18.
+//  Copyright © 2018 Masai Young. All rights reserved.
+//
+
+import Foundation
+
+struct Flashcard {}
---
+++ FlashcardHW/Views/View.swift
@@ -0,0 +1,9 @@
+//
+//  View.swift
+//  FlashcardHW
+//
+//  Created by Masai Young on 2/12/18.
+//  Copyright © 2018 Masai Young. All rights reserved.
+//
+
+import Foundation
